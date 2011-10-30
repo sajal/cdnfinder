f0a70000ed0d459c0883a36a6cff431b10157768
Sajal Kayan <sajal83@gmail.com>
2011-10-31 01:46:35 +0700

    initial commit

---
+++ cdnfinder.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import dns
+import dns.message
+import dns.query
+
+CDN_PROVIDER  = [
+    [".akamai.net", "Akamai"],
+    [".akamaiedge.net", "Akamai"],
+    [".llnwd.net", "Limelight"],
+    ["edgecastcdn.net", "Edgecast"],
+    ["hwcdn.net", "Highwinds"],
+    [".panthercdn.com", "Panther"],
+    [".simplecdn.net", "Simple CDN"],
+    [".instacontent.net", "Mirror Image"],
+    [".footprint.net", "Level 3"],
+    [".ay1.b.yahoo.com", "Yahoo"],
+    [".yimg.", "Yahoo"],
+    [".google.", "Google"],
+    ["googlesyndication.", "Google"],
+    ["youtube.", "Google"],
+    [".googleusercontent.com", "Google"],
+    [".internapcdn.net", "Internap"],
+    [".cloudfront.net", "Amazon CloudFront"],
+    [".netdna-cdn.com", "MaxCDN"],
+    [".netdna-ssl.com", "MaxCDN"],
+    [".netdna.com", "MaxCDN"],
+    [".cotcdn.net", "Cotendo CDN"],
+    [".cachefly.net", "Cachefly"],
+    ["bo.lt", "BO.LT"],
+    [".cloudflare.com", "Cloudflare"],
+    [".afxcdn.net", "afxcdn.net"],
+    [".lxdns.com", "lxdns.com"],
+    [".att-dsa.net", "AT&T"],
+    [".vo.msecnd.net", "Windows Azure"],
+    [".voxcdn.net", "VoxCDN"],
+    [".bluehatnetwork.com", "Blue Hat Network"],
+    [".swiftcdn1.com", "SwiftCDN"],
+    [".rncdn1.com", "Reflected Networks"],
+    [".cdngc.net", "CDNetworks"],
+    [".fastly.net", "Fastly"],
+    [".gslb.taobao.com", "Taobao"],
+    [".gslb.tbcache.com", "Alimama"]
+]
+
+def finder(host):
+    result = None
+    for cdn in CDN_PROVIDER:
+        if cdn[0] in host:
+            return cdn[1]
+    return None
+
+def findcdnfromhost(host):
+    newhost = host
+    try:
+        dnsip = "8.8.8.8"
+        q = dns.message.make_query(host, "A")
+        r = dns.query.udp(q, dnsip)
+        for ans in r.answer:
+            if "CNAME" in ans.to_text():
+                newhost = ans.to_text().split("CNAME ")[1][:-1]
+    except:
+        return finder(newhost)
+    return finder(newhost)
+
+if __name__ == "__main__":
+    import sys
+    print findcdnfromhost(sys.argv[-1])
